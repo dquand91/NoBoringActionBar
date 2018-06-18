4bcfa85468d0953aebb51ee575c9d6e96c8a0050
dquand91 <dquand91@gmail.com>
2018-06-18 17:30:00 +0700

    - First commit

---
+++ app/src/main/java/luongduongquan/com/noboringactionbaractivity/MainActivity.java
@@ -0,0 +1,22 @@
+package luongduongquan.com.noboringactionbaractivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, NoBoringActionBarActivity.class));
+			}
+		});
+	}
+}
